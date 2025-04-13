4213dca29901da24cb4c04113083c57a2a096d0f
cozfuttu <canozfuttu07@gmail.com>
2025-04-13 20:09:39 +0300

    feat: added revalidation endpoint

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
+
+export async function POST(req: NextRequest) {
+  try {
+    if (req.headers.get("Revalidation-Key") !== process.env.REVALIDATION_KEY) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    revalidateTag("revalidate");
+
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
